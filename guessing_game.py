35b113b1628fd8be38e1ed0e08765b33417c72c7
Mayble <mehburohit@gmail.com>
2023-10-28 01:12:59 -0400

    guessing game hard and easy level implemented

--- guessing_game.py
+++ guessing_game.py
@@ -1,24 +1,55 @@
-
 import random
 
 # introduce audience to the game
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 10.")
+print("I'm thinking of a number between 1 and 100.")
 level = input(("Choose a difficulty. Type 'easy' or 'hard': "))
-attempts = 5
-print(f"You have {attempts} attempts remaining to guess the number.")
-guess = int(input("Make a guess: "))
-random_num = random.randint(1, 10)
-print(random_num)
 
+# Number Of Lives
 if level == 'hard':
-    while guess > random_num or guess < random_num:
+    attempts = 5
+elif level == 'easy':
+    attempts = 10
+
+# Pick a random number to store
+random_num = random.randint(1, 100)
+
+# Hard Level
+if level == 'hard':
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
         if guess > random_num:
             print("Too High!")
+            attempts -= 1
+            if attempts == 0: 
+                print(f"You've run out of guesses, you lose! The answer was {random_num}")
         elif guess < random_num:
             print("Too Low!!")
-        attempts -= 1
-        print(f"you have {attempts} attempts remaining to guess the number.")
+            attempts -= 1
+            if attempts == 0: 
+                print(f"You've run out of guesses, you lose! The answer was {random_num}")
+        else:
+            print("You won!")
+            attempts = 0
+        
+elif level == 'easy':
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
-    
-print(f"You got it! The answer was {random_num}")   
+        if guess > random_num:
+            print("Too High!")
+            attempts -= 1
+            if attempts == 0: 
+                print(f"You've run out of guesses, you lose! The answer was {random_num}")
+        elif guess < random_num:
+            print("Too Low!!")
+            attempts -= 1
+            if attempts == 0: 
+                print(f"You've run out of guesses, you lose! The answer was {random_num}")
+        else:
+            print("You won!")
+            attempts = 0
+        
+
+ 
